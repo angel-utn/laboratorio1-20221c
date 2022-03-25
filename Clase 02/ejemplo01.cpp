d9bfdf2f8b921b099d9ff9d8f5fefea8056eda81
Angel <asimon@docentes.frgp.utn.edu.ar>
2022-03-25 20:53:36 -0300

    Clase 02 - Archivos fuente

---
+++ Clase 02/ejemplo01.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    int numero;
+    cout << "Ingresar un número: ";
+    cin >> numero;
+
+    numero *= 2; // numero = numero * 2;
+
+    cout << "Resultado: " << numero;
+
+    return 0;
+}
